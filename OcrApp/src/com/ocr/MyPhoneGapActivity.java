ec6cfcaf192c011c73c2800eb400f6b9f70e58ed
Ferchurch <fernando@iMac-de-Fernando.local>
2015-01-26 16:53:04 +0100

    It reads an image and reads the text not excellently
    I have to reach threshold effect and calculate the quantity of black
    and white pixels to invert or not the inner image and getting the text
    better

---
+++ OcrApp/src/com/ocr/MyPhoneGapActivity.java
@@ -0,0 +1,12 @@
+package com.ocr;
+
+import  com.phonegap.*;
+import android.os.Bundle;
+
+public class MyPhoneGapActivity extends DroidGap {
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		super.loadUrl("file:///android_asset/www/index.html");
+	}
+}
